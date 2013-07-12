1cd7909b81ff24063f359b332d683e9b57ee3b3b
binitdatta@gmail.com <binitdatta@gmail.com@cfa0812e-1e33-9972-390c-77d14af84728>
2013-07-12 18:43:13 +0000

    Share project "spring-ws-recipes-client" into "https://rollingstonesprngmvc.googlecode.com/svn"

---
+++ spring-ws-recipes-client/src/main/java/com/rollingstone/recipes/domain/Recipe.java
@@ -0,0 +1,129 @@
+package com.rollingstone.recipes.domain;
+
+import java.sql.Blob;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+
+@XmlRootElement(name = "recipe")
+@Entity
+@Table(name="RECIPE")
+public class Recipe {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="RECIPEID")
+	int recipeId;
+	
+	@Column(name="RECIPE_NAME")
+	String recipeName;
+	
+	@Column(name="DESCRIPTION")
+	String recipeDescripton;
+	
+	@Column(name="CREATED_ON")
+	Date createdOn;
+	
+	@Column(name="CREATED_BY")
+	String createdBy;
+	
+	@Column(name="RECIPETYPE")
+	String recipeType;
+	
+	@Column(name="VISITOR_COUNT")
+	int visitorCount;
+	
+	@Column(name="PROCESS")
+	Blob process;
+
+	@OneToMany(mappedBy="recipe", fetch=FetchType.EAGER)
+	@Cascade(value = { CascadeType.ALL })
+	@Fetch(FetchMode.SUBSELECT)
+	List<RecipeIngredients> ingredients;
+
+	public int getRecipeId() {
+		return recipeId;
+	}
+
+	public void setRecipeId(int recipeId) {
+		this.recipeId = recipeId;
+	}
+
+	public String getRecipeName() {
+		return recipeName;
+	}
+
+	public void setRecipeName(String recipeName) {
+		this.recipeName = recipeName;
+	}
+
+	public String getRecipeDescripton() {
+		return recipeDescripton;
+	}
+
+	public void setRecipeDescripton(String recipeDescripton) {
+		this.recipeDescripton = recipeDescripton;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getRecipeType() {
+		return recipeType;
+	}
+
+	public void setRecipeType(String recipeType) {
+		this.recipeType = recipeType;
+	}
+
+	public int getVisitorCount() {
+		return visitorCount;
+	}
+
+	public void setVisitorCount(int visitorCount) {
+		this.visitorCount = visitorCount;
+	}
+
+	public Blob getProcess() {
+		return process;
+	}
+
+	public void setProcess(Blob process) {
+		this.process = process;
+	}
+
+	public List<RecipeIngredients> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(List<RecipeIngredients> ingredients) {
+		this.ingredients = ingredients;
+	}
+}
