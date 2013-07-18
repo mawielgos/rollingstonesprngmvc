9b9ee4a880c70736da335a832cd00df467276484
binitdatta@gmail.com <binitdatta@gmail.com@cfa0812e-1e33-9972-390c-77d14af84728>
2013-07-18 12:13:30 +0000

    Created by wsimport

---
+++ trunk/spring-ws-recipes-client/src/main/java/com/rollingstone/recipes/oxm/Recipe.java
@@ -0,0 +1,244 @@
+
+package com.rollingstone.recipes.oxm;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element ref="{http://binit.blogspot.com/ws/schema/recipe}recipeId"/>
+ *         &lt;element ref="{http://binit.blogspot.com/ws/schema/recipe}recipeName"/>
+ *         &lt;element ref="{http://binit.blogspot.com/ws/schema/recipe}recipeDescripton"/>
+ *         &lt;element ref="{http://binit.blogspot.com/ws/schema/recipe}createdBy"/>
+ *         &lt;element ref="{http://binit.blogspot.com/ws/schema/recipe}recipeType"/>
+ *         &lt;element ref="{http://binit.blogspot.com/ws/schema/recipe}visitorCount"/>
+ *         &lt;element ref="{http://binit.blogspot.com/ws/schema/recipe}process"/>
+ *         &lt;sequence>
+ *           &lt;element ref="{http://binit.blogspot.com/ws/schema/recipe}recipe_ingredients"/>
+ *         &lt;/sequence>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "recipeId",
+    "recipeName",
+    "recipeDescripton",
+    "createdBy",
+    "recipeType",
+    "visitorCount",
+    "process",
+    "recipeIngredients"
+})
+@XmlRootElement(name = "recipe")
+public class Recipe {
+
+    protected int recipeId;
+    @XmlElement(required = true)
+    protected String recipeName;
+    @XmlElement(required = true)
+    protected String recipeDescripton;
+    @XmlElement(required = true)
+    protected String createdBy;
+    @XmlElement(required = true)
+    protected String recipeType;
+    protected int visitorCount;
+    @XmlElement(required = true)
+    protected String process;
+    @XmlElement(name = "recipe_ingredients", required = true)
+    protected RecipeIngredients recipeIngredients;
+
+    /**
+     * Gets the value of the recipeId property.
+     * 
+     */
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    /**
+     * Sets the value of the recipeId property.
+     * 
+     */
+    public void setRecipeId(int value) {
+        this.recipeId = value;
+    }
+
+    /**
+     * Gets the value of the recipeName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    /**
+     * Sets the value of the recipeName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setRecipeName(String value) {
+        this.recipeName = value;
+    }
+
+    /**
+     * Gets the value of the recipeDescripton property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getRecipeDescripton() {
+        return recipeDescripton;
+    }
+
+    /**
+     * Sets the value of the recipeDescripton property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setRecipeDescripton(String value) {
+        this.recipeDescripton = value;
+    }
+
+    /**
+     * Gets the value of the createdBy property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    /**
+     * Sets the value of the createdBy property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCreatedBy(String value) {
+        this.createdBy = value;
+    }
+
+    /**
+     * Gets the value of the recipeType property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getRecipeType() {
+        return recipeType;
+    }
+
+    /**
+     * Sets the value of the recipeType property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setRecipeType(String value) {
+        this.recipeType = value;
+    }
+
+    /**
+     * Gets the value of the visitorCount property.
+     * 
+     */
+    public int getVisitorCount() {
+        return visitorCount;
+    }
+
+    /**
+     * Sets the value of the visitorCount property.
+     * 
+     */
+    public void setVisitorCount(int value) {
+        this.visitorCount = value;
+    }
+
+    /**
+     * Gets the value of the process property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getProcess() {
+        return process;
+    }
+
+    /**
+     * Sets the value of the process property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setProcess(String value) {
+        this.process = value;
+    }
+
+    /**
+     * Gets the value of the recipeIngredients property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link RecipeIngredients }
+     *     
+     */
+    public RecipeIngredients getRecipeIngredients() {
+        return recipeIngredients;
+    }
+
+    /**
+     * Sets the value of the recipeIngredients property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link RecipeIngredients }
+     *     
+     */
+    public void setRecipeIngredients(RecipeIngredients value) {
+        this.recipeIngredients = value;
+    }
+
+}
